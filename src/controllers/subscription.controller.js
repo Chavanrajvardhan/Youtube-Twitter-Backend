354f245bff26ba0e384b7877a0438160a44119ff
Raj <112115798+Chavanrajvardhan@users.noreply.github.com>
2024-05-04 09:32:01 +0530

    add toggleSubscription functionality

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,19 +1,50 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { User } from "../models/user.model.js"
 import { Subscription } from "../models/subscription.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const { channelId } = req.params
     // TODO: toggle subscription
+
+    if (!channelId) {
+        throw new ApiError(400, "ChanelId Missing")
+    }
+
+    const isSubscribed = await Subscription.findById(
+        {
+            subscriber: req.users?._id,
+            channel: channelId
+        }
+    )
+
+    if (isSubscribed) {
+        await Subscription.findByIdAndDelete({ isSubscribed: _id })
+
+        return res.status(200).json(
+            new ApiResponse("200", { isSubscribed: false }, "Unsubscribed")
+        )
+    }
+
+    await Subscription.create(
+        {
+            subscriber: req.users?._id,
+            channel: channelId 
+        }
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200, {isSubscribed: true}, "Subscribed Successfully")
+    )
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const { channelId } = req.params
 })
 
 // controller to return channel list to which user has subscribed
