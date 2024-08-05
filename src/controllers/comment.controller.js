720ee397f030a6762cfc778d1e3a978c856a545e
Raj <112115798+Chavanrajvardhan@users.noreply.github.com>
2024-08-05 14:51:22 +0530

    resolve errors in get videoComments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -7,6 +7,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const { videoId } = req.params
+    // console.log(videoId)
     const { page = 1, limit = 10 } = req.query
 
     if (!isValidObjectId(videoId)) {
@@ -16,28 +17,36 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const parsedPage = Number(page);
     const parsedLimit = Number(limit);
 
-    if(!Number.isFinite(page)){
+    if(!Number.isFinite(parsedPage)){
         throw new ApiError(400, "page is required")
     }
 
-    if(!Number.isFinite(limit)){
+    if(!Number.isFinite(parsedLimit)){
         throw new ApiError(400, "Limit is required")
     }
 
-    const getVideoComments = await Comment.aggregate([
+    const aggregationPipeline = [
         {
             $match: {
-                _id :new mongoose.Types.ObjectId(videoId)
+                video: new mongoose.Types.ObjectId(videoId)
             }
+        },
+        {
+            $sort: { createdAt: -1 } // Optional: sort comments by creation date
         }
-    ])
-
+    ];
     const options = {
         page : parsedPage,
         limit: parsedLimit
     }
+    
+    console.log(options)
+    const comments = await Comment.aggregatePaginate(
+        Comment.aggregate(aggregationPipeline),
+        options
+    )
+   
 
-    const comments = await Comment.aggregatePaginate({aggregate: getVideoComments,options})
     return res.status(200).json(
         new ApiResponse(200, comments, "All Video Comment Fetched Successfully")
     )
@@ -92,7 +101,6 @@ const updateComment = asyncHandler(async (req, res) => {
 
     const updateComment = await Comment.findByIdAndUpdate(
         { _id: commentId },
-
         {
             $set: { content }
         }
